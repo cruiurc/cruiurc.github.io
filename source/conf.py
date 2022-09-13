fde1a21d89cf30ea3e1d66485177df672e41db26
cruiurc <crui@outlook.com>
2022-09-13 18:11:36 +0800

    config - plantuml

--- source/conf.py
+++ source/conf.py
@@ -27,9 +27,9 @@ author = 'cr'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'plantweb.directive',
-]
+# extensions = [
+#     'plantweb.directive',
+# ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
