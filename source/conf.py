ade8ef5cb8e343f691e0e55a290805a8704d0821
cruiurc <crui@outlook.com>
2022-09-14 09:10:44 +0800

    1

--- source/conf.py
+++ source/conf.py
@@ -45,7 +45,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -53,5 +53,5 @@ exclude_patterns = []
 html_static_path = ['_static']
 
 
-html_theme = 'agogo'
+
 language = 'zh_CN'
