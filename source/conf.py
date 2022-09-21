5534a404ddeab2f694a983e9eebbe73e45968699
cruiurc <crui@outlook.com>
2022-09-21 14:41:55 +0800

    add graphviz

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,20 @@ author = 'cr'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+# extensions = [
+# ]
+# 通过配置开启graphviz插件
+extensions = ['sphinx.ext.graphviz']
+
+# 设置 graphviz_dot 路径
+graphviz_dot = 'dot'
+# 设置 graphviz_dot_args 的参数，这里默认了默认字体
+graphviz_dot_args = ['-Gfontname=Georgia',
+                     '-Nfontname=Georgia',
+                     '-Efontname=Georgia']
+# 输出格式，默认png，这里我用svg矢量图
+graphviz_output_format = 'svg'
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,3 +64,4 @@ html_theme = 'bizstyle'
 html_static_path = ['_static']
 
 language = 'zh_CN'
+
